302cfc87d2b9da181309f3abb714e2640ac0e6d2
Serge Nevsky <sergey@sergeyk.com>
2016-03-30 22:57:19 -0500

    Serving a site with jade and stylus.

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World from the Socialowl app\n');
-}).listen(port);
+var express = require('express')
+  , logger = require('morgan')
+  , app = express()
+  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade');
+
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function (req, res, next) {
+  try {
+    var html = template({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+});
