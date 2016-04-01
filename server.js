b040e2f7acf1581a448cf933d952624b0b443b71
Serge Nevsky <sergey@sergeyk.com>
2016-03-31 22:19:42 -0500

    Authentication progress.

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 var express = require('express')
+  , path = require('path')
   , logger = require('morgan')
   , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade');
+  , mongoose = require('mongoose')
+  , passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy
+  , routes = require('./routes/index');
+
+app.set('views', path.join(__dirname, 'source/templates'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(__dirname + '/static'));
 
-app.get('/', function (req, res, next) {
-  try {
-    var html = template({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
-});
+app.use('/', routes);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
-});
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+mongoose.connect('mongodb://localhost/personowl');
+
+module.exports = app;
