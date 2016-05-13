505d1d667cb898ed22a1822521098aa47ce1b800
Serge Nevsky <sergey@sergeyk.com>
2016-05-12 22:37:17 -0500

    Contacts list/lscontact now SPA.

--- routes/api.js
+++ routes/api.js
@@ -24,10 +24,12 @@ router.get('/contacts', isAuthenticated, function(req, res) {
 router.get('/contacts/:id', isAuthenticated, function(req, res) {
   var query = { "_id" : req.params.id };
   Contact.findOne(query, function(err, contact) {
-    res.render(
-      'contact',
-      { contact : contact }
-    );
+    res.status(200)
+      .json({
+        status: 'success',
+        contact: contact,
+        message: 'Retrieved ONE contact'
+      });
   });
 });
 
@@ -50,58 +52,88 @@ router.put('/contacts/:id', isAuthenticated, function(req, res) {
   var update = { name : req.body.name, tags : tags };
   var options = { new: true };
   Contact.findOneAndUpdate(query, update, options, function(err, contact){
-    res.render(
-      'contact',
-      { contact : contact }
-    );
+    res.status(200)
+      .json({
+        status: 'success',
+        contact: contact,
+        message: 'Updated a contact'
+      });
   });
 });
 
 router.delete('/contacts/:id', isAuthenticated, function(req, res) {
   var query = { "_id" : req.params.id };
-  Contact.findOneAndRemove(query, function(err, contact){
-    res.redirect('/contacts');
+  Contact.findOneAndRemove(query, function(){
+    res.status(200)
+      .json({
+        status: 'success',
+        message: 'Deleted a contact'
+      });
   });
 });
 
 router.post('/contacts/:id/interactions', isAuthenticated, function(req, res) {
   var today = Date.now();
+  var options = { new: true };
   Contact.findByIdAndUpdate(req.params.id, {
     $push: {
-      interactions: { description: req.body.description, account: req.user._id }
+      interactions: { description: req.body.interaction, account: req.user._id }
     }, lastContactAt: today
-  }, {}, function(err, contact){
-    res.redirect('/api/contacts/' + req.params.id);
+  }, options, function(err, contact){
+    res.status(200)
+      .json({
+        status: 'success',
+        interactions: contact.interactions,
+        message: 'Added a new interaction'
+      });
   });
 });
 
 router.delete('/contacts/:id/interactions/:interactionId', isAuthenticated, function(req, res) {
+  var options = { new: true };
   Contact.findByIdAndUpdate(req.params.id, {
     $pull: {
       interactions: { _id: req.params.interactionId }
     }
-  }, {}, function(err, contact){
-    res.redirect('/api/contacts/' + req.params.id);
+  }, options, function(err, contact){
+    res.status(200)
+      .json({
+        status: 'success',
+        interactions: contact.interactions,
+        message: 'Deleted an interaction'
+      });
   });
 });
 
 router.post('/contacts/:id/notes', isAuthenticated, function(req, res) {
+  var options = { new: true };
   Contact.findByIdAndUpdate(req.params.id, {
     $push: {
-      notes: { body: req.body.body, account: req.user._id }
+      notes: { body: req.body.note, account: req.user._id }
     }
-  }, {}, function(err, contact){
-    res.redirect('/api/contacts/' + req.params.id);
+  }, options, function(err, contact){
+    res.status(200)
+      .json({
+        status: 'success',
+        notes: contact.notes,
+        message: 'Added a new note'
+      });
   });
 });
 
 router.delete('/contacts/:id/notes/:noteId', isAuthenticated, function(req, res) {
+  var options = { new: true };
   Contact.findByIdAndUpdate(req.params.id, {
     $pull: {
       notes: { _id: req.params.noteId }
     }
-  }, {}, function(err, contact){
-    res.redirect('/api/contacts/' + req.params.id);
+  }, options, function(err, contact){
+    res.status(200)
+      .json({
+        status: 'success',
+        notes: contact.notes,
+        message: 'Deleted a note'
+      });
   });
 });
 
--- static/js/app.js
+++ static/js/app.js
@@ -11,10 +11,17 @@
         templateUrl: '/views/contactsListView.html'
       });
 
+      $routeProvider.when('/:id', {
+        controller: 'contactController',
+        controllerAs: 'vm',
+        templateUrl: '/views/contactView.html'
+      });
+
       $routeProvider.otherwise({ redirectTo: '/' });
 
     })
-    .controller('contactsController', contactsController);
+    .controller('contactsController', contactsController)
+    .controller('contactController', contactController);
 
   function contactsController($http) {
 
@@ -64,6 +71,118 @@
             contact.lastContactMessage += 'never';
         }
       });
+    }
+
+  }
+
+  function contactController($routeParams, $http, $location) {
+
+    var vm = this;
+    var id = $routeParams.id;
+
+    vm.isBusy = true;
+    vm.isSaving = false;
+    vm.contact = {};
+    vm.errorMessage = '';
+    vm.newInteraction = '';
+    vm.newNote = '';
+
+    $http.get('/api/contacts/' + id)
+      .then(function(response) {
+        setContact(response.data.contact);
+      }, function(error) {
+        vm.errorMessage = 'Failed to load contact. ';
+        vm.errorMessage += error;
+      })
+      .finally(function() {
+        vm.isBusy = false;
+      });
+
+    vm.saveChanges = function() {
+      vm.isSaving = true;
+      vm.errorMessage = '';
+
+      var url = '/api/contacts/' + id + '?_method=PUT';
+      $http.post(url, vm.contact)
+        .then(function (response) {
+          setContact(response.data.contact);
+        }, function (error) {
+          vm.errorMessage = 'Failed to save changes to contact. ';
+          vm.errorMessage += error;
+        })
+        .finally(function () {
+          vm.isSaving = false;
+        });
+    };
+
+    vm.addNote = function() {
+      var url = '/api/contacts/' + id + '/notes';
+      var data = { note: vm.newNote };
+      $http.post(url, data)
+        .then(function(response) {
+          angular.copy(response.data.notes, vm.contact.notes);
+          vm.newNote = '';
+        }, function() {
+          vm.errorMessage = 'Failed to add a new note.';
+        });
+    };
+
+    vm.deleteNote = function(note) {
+      var url = '/api/contacts/' + id + '/notes/' + note._id + '?_method=DELETE';
+      $http.post(url, vm.contact)
+        .then(function (response) {
+          angular.copy(response.data.notes, vm.contact.notes);
+        }, function (error) {
+          vm.errorMessage = 'Failed to delete note. ';
+          vm.errorMessage += error;
+        });
+    };
+
+    vm.addInteraction = function() {
+      var url = '/api/contacts/' + id + '/interactions';
+      var data = { interaction: vm.newInteraction };
+      $http.post(url, data)
+        .then(function(response) {
+          angular.copy(response.data.interactions, vm.contact.interactions);
+          vm.newInteraction = '';
+        }, function() {
+          vm.errorMessage = 'Failed to add a new interaction.';
+        });
+    };
+
+    vm.deleteInteraction = function(interaction) {
+      var url = '/api/contacts/' + id + '/interactions/' + interaction._id + '?_method=DELETE';
+      $http.post(url, vm.contact)
+        .then(function (response) {
+          angular.copy(response.data.interactions, vm.contact.interactions);
+        }, function (error) {
+          vm.errorMessage = 'Failed to delete interaction. ';
+          vm.errorMessage += error;
+        });
+    };
+
+    vm.deleteContact = function() {
+      var url = '/api/contacts/' + id + '?_method=DELETE';
+      $http.post(url, vm.contact)
+        .then(function () {
+          $location.path('/');
+        }, function (error) {
+          vm.errorMessage = 'Failed to delete contact. ';
+          vm.errorMessage += error;
+        });
+    };
+
+    function setContact(contact) {
+      contact.tags = contact.tags.join(',');
+      contact.lastContactMessage = getFormattedDate(contact.lastContactAt);
+      angular.copy(contact, vm.contact);
+    }
+
+    function getFormattedDate(lastContactAt){
+      if (lastContactAt)
+        return moment(lastContactAt).fromNow();
+      else
+        return 'Never';
     }
 
   }
